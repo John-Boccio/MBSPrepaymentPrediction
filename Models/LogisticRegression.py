d99a70011bcc8552bcaa94ad0a42564fe475e76b
John Boccio <john.t.boccio@gmail.com>
2021-05-23 21:26:23 -0400

    Fixed logistic regression code

--- Models/LogisticRegression.py
+++ Models/LogisticRegression.py
@@ -20,15 +20,15 @@ test_y = test['zero_balance_code']
 test_x = test.drop(columns='zero_balance_code')
 
 logreg = sklearn.linear_model.LogisticRegression(max_iter=10000, solver='liblinear', random_state=seed)
-logreg.fit(train, train_prepayment)
-score_train = logreg.score(train, train_prepayment)
+logreg.fit(train_x, train_y)
+score_train = logreg.score(train_x, train_y)
 print(score_train)
-score_test = logreg.score(test, test_prepayment)
+score_test = logreg.score(test_x, test_y)
 print(score_test)
 
 predictions = logreg.predict(test)
-sklearn.metrics.plot_confusion_matrix(logreg, test, test_prepayment, cmap='Blues')
+sklearn.metrics.plot_confusion_matrix(logreg, test_x, test_y, cmap='Blues')
 plt.show()
-classification_report = sklearn.metrics.classification_report(test_prepayment, predictions)
+classification_report = sklearn.metrics.classification_report(test_y, predictions)
 print(classification_report)
 
